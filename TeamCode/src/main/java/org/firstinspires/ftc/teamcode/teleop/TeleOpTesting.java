06d8638cbed75e3c7ad53af3872736dd0796eebc
ftcaarya <rautaarya23@gmail.com>
2025-06-28 01:25:29 -0400

    added teleop testing class for funs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpTesting.java
@@ -10,34 +10,36 @@ import org.firstinspires.ftc.teamcode.extraneous.AllMechs;
 public class TeleOpTesting extends LinearOpMode {
     AllMechs robot;
 
+
     @Override
     public void runOpMode() throws InterruptedException {
-        robot = new AllMechs(hardwareMap, 0, 0);
+            robot = new AllMechs(hardwareMap, 0, 0);
 
-        waitForStart();
+            waitForStart();
 
-        while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
+            while (opModeIsActive()) {
+                double y = -gamepad1.left_stick_y;
+                double x = gamepad1.left_stick_x;
+                double rx = gamepad1.right_stick_x;
 
-            double frontLeftPower = (y + x + rx);
-            double rearLeftPower = (y - x + rx);
-            double frontRightPower = (y - x - rx);
-            double rearRightPower = (y + x - rx);
+                double frontLeftPower = (y + x + rx);
+                double rearLeftPower = (y - x + rx);
+                double frontRightPower = (y - x - rx);
+                double rearRightPower = (y + x - rx);
 
-            frontLeftPower = Range.clip(frontLeftPower, -1, 1);
-            rearLeftPower = Range.clip(rearLeftPower, -1, 1);
-            frontRightPower = Range.clip(frontRightPower, -1 , 1);
-            rearRightPower = Range.clip(rearRightPower, -1, 1);
+                frontLeftPower = Range.clip(frontLeftPower, -1, 1);
+                rearLeftPower = Range.clip(rearLeftPower, -1, 1);
+                frontRightPower = Range.clip(frontRightPower, -1, 1);
+                rearRightPower = Range.clip(rearRightPower, -1, 1);
 
-            robot.frontLeft.setPower(frontLeftPower);
-            robot.rearLeft.setPower(rearLeftPower);
-            robot.frontRight.setPower(frontRightPower);
-            robot.rearRight.setPower(rearRightPower);
+                robot.frontLeft.setPower(frontLeftPower);
+                robot.rearLeft.setPower(rearLeftPower);
+                robot.frontRight.setPower(frontRightPower);
+                robot.rearRight.setPower(rearRightPower);
 
+            }
 
         }
 
     }
-}
+
